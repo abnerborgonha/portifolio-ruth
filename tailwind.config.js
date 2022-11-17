2faf760aaede96c21a9d224a3d2f28e1502726c2
Abnêr Borgonha <abnerborgonha@gmail.com>
2022-11-17 17:21:01 -0300

    style(theme): add color style
    set theme color style by aplication

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f7bd2d',
+        background: '#dedede',
+        text: '#242424',
+        transparent: 'transparent'
+      }
+    },
   },
   plugins: [],
 }
